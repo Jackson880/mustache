2c2fe4e12bae85909889d0565f28ba659210ad16
Jackson880 <84539099+Jackson880@users.noreply.github.com>
2022-05-07 14:51:44 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,4 @@
-noseX=0;
-noseY=0;
-
 function preload() {
-  clown_nose = loadImage('https://i.postimg.cc/44MjSm6K/mustache-student-math-favorite-for-friday-the-the-1.png');
 }
 
 function setup() {
@@ -11,28 +7,14 @@ function setup() {
   video = createCapture(VIDEO);
   video.size(300, 300);
   video.hide();
-
-  poseNet = ml5.poseNet(video, modelLoaded);
-  poseNet.on('pose', gotPoses);
 }
 
 function modelLoaded() {
-  console.log('PoseNet Is Initialized');
-}
-
-function gotPoses(results)
-{
-  if(results.length > 0)
-  {
-    console.log(results);
-    noseX = results[0].pose.nose.x-15;
-    noseY = results[0].pose.nose.y-15;
-  }
 }
 
 function draw() {
-  image(video, 0, 0, 300, 300);
-  image(clown_nose, noseX, noseY, 30, 30);
+  image(video, 0, 0, 1, 1);
+
 }
 
 function take_snapshot(){    
