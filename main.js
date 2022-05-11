595d0c191572ae0a2ddb0e67fb8f8a90428d02c4
Jackson880 <84539099+Jackson880@users.noreply.github.com>
2022-05-10 17:30:57 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -2,21 +2,34 @@ function preload() {
 }
 
 function setup() {
-  canvas = createCanvas(300, 300);
-  canvas.center();
-  video = createCapture(VIDEO);
-  video.size(300, 300);
-  video.hide();
+canvas = createCanvas(300, 300);
+canvas.center();
+video = createCapture(VIDEO);
+video.size(300, 300);
+video.hide();
+
+poseNet = ml5.poseNet(video, modelLoaded);
+poseNet.on('pose', gotPoses);
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        console.log("mustache x = " + results[0].pose.nose.x);
+        console.log("mustache y = " + results[0].pose.nose.y);
+    }
 }
 
 function modelLoaded() {
+    console.log('poseNet is Initalized');
 }
 
 function draw() {
-  image(video, 0, 0, 1, 1);
-
+    image(video, 0, 0, 300, 300);
 }
-
-function take_snapshot(){    
-  save('myFilterImage.png');
-}
+    
+function take_snapshot(){
+    save('MustacheImage.png');
+    }
